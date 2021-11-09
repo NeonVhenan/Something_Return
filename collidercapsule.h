553215865f82a2eeb425f3c156758c09b254d488
Mérédith CERESOLE <45624502+Mceresole@users.noreply.github.com>
2021-11-09 12:57:52 +0100

    Classe Collider pour collision

---
+++ collidercapsule.h
@@ -0,0 +1,19 @@
+#ifndef COLLIDERCAPSULE_H
+#define COLLIDERCAPSULE_H
+
+#include "collider.h"
+
+class ColliderCapsule : public Collider
+{
+public:
+    ColliderCapsule();
+    ColliderCapsule(QVector3D point1, QVector3D point2, float rayon);
+
+    bool collision(ColliderBox c);
+
+    QVector3D point1;
+    QVector3D point2;
+    float rayon;
+};
+
+#endif // COLLIDERCAPSULE_H
