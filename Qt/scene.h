b947cecd3d76be38991d5c88d47ee03c6a75f7dd
LWK-Malika <64968935+LWK-Malika@users.noreply.github.com>
2021-11-22 17:34:11 +0100

    Add files via upload

--- Qt/scene.h
+++ Qt/scene.h
@@ -1,11 +1,23 @@
 #ifndef SCENE_H
 #define SCENE_H
 
+#include "gameobject.h"
 
-class scene
+class Scene
 {
+private:
+    QString *name;
+    QList *objectList;
+
 public:
-    scene();
+    Scene();
+    virtual ~Scene();
+   // Scene(GameObject *object);
+
+  //  void generateListObject(QList *sceneObjects);
+
+
+
 };
 
 #endif // SCENE_H
